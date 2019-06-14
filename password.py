2fdb13d21f53bb3049a566fa2c1b20fc11f5a31e
Emma Sharon <emmasharon19@gmail.com>
2019-06-14 16:57:05 +0300

    create password.py file

---
+++ password.py
@@ -0,0 +1,11 @@
+class User:
+    '''
+    Class that generates new instance of passwords
+    '''
+
+class Credentials:
+    '''
+    Class to let user add save or delete passwords
+    '''
+
+    
