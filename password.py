bf2b28059da725590fe036c00539bacb55c47159
Emma Sharon <emmasharon19@gmail.com>
2019-06-14 17:11:48 +0300

    create user class and credentials class

--- password.py
+++ password.py
@@ -2,10 +2,16 @@ class User:
     '''
     Class that generates new instance of passwords
     '''
-
+    # def __init_(self,first_name,last_name,password):
+    #     '''
+    #     __init__ method that helps us define properties for our objects.
+    #
+    #     Args:
+    #         first_name: New user first name.
+    #         last_name: New user last name.
+    #         password: New password for the created account.
+    #     '''
 class Credentials:
     '''
     Class to let user add save or delete passwords
     '''
-
-    
